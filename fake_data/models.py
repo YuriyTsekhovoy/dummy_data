a4a17df1577f64fa05912a23fa74692797bf4a79
Юрий Цеховой <yuriy.tsekhovoy@gmail.com>
2020-12-13 14:36:08 +0200

    fakedatamodel update

--- fake_data/models.py
+++ fake_data/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class FakeDataModel(models.Model):
     model = models.ForeignKey('SchemaDataModel', on_delete=models.CASCADE)
-    file_link = models.URLField(
-        max_length=200, db_index=True, unique=True, blank=True)
+    file = models.FileField(upload_to='files')
 
 
 class SchemaDataModel(models.Model):
